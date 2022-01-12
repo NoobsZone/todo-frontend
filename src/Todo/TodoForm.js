da0583a7c310cc75b5a47c22b64362263bcfc459
Anoy <anoy.eatl@gmail.com>
2022-01-12 18:58:04 +0600

    updated

--- src/Todo/TodoForm.js
+++ src/Todo/TodoForm.js
@@ -1,11 +1,66 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
 
-function TodoForm() {
-    return (
-        <div>
-            
-        </div>
-    )
+function TodoForm(props) {
+  const [todos, setTodos] = useState('');
+  const api = "http://localhost:5000/api/v1/todos";
+
+  const inputRef = useRef(null); // Ref Using
+
+  useEffect(() => {
+    inputRef.current.focus();
+  });
+
+  async function postTodos(e) {
+    const newTodo = {
+      name:todos,
+    };
+    e.preventDefault();
+    await axios.post(api, newTodo);
+    console.log(newTodo);
+  }
+
+  const handleChange = (e) => {
+    setTodos(e.target.value);
+  };
+
+
+
+  return (
+    <div>
+      <form className="todo-form" onSubmit={postTodos}>
+        {props.edit ? (
+          <>
+            <input
+              placeholder="Update todo"
+              value={todos}
+              onChange={handleChange}
+              name="text"
+              ref={inputRef}
+              className="todo-input edit"
+            />
+            <button onClick={postTodos} className="todo-button edit">
+              Update
+            </button>
+          </>
+        ) : (
+          <>
+            <input
+              placeholder="Add a todo"
+              value={todos}
+              onChange={handleChange}
+              name="text"
+              className="todo-input"
+              ref={inputRef}
+            />
+            <button onClick={postTodos} className="todo-button">
+              Add todo
+            </button>
+          </>
+        )}
+      </form>
+    </div>
+  );
 }
 
-export default TodoForm
+export default TodoForm;
--- src/Todo/TodoList.js
+++ src/Todo/TodoList.js
@@ -1,30 +1,33 @@
-import React from 'react'
-import axios from 'axios';
-import {useState, useEffect} from 'react';
+import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+import TodoForm from "./TodoForm";
 
 function TodoList() {
-const [todos,setTodos]=useState([]);
-const api ='http://localhost:5000/api/v1/todos';
+  const [todos, setTodos] = useState([]);
+  const api = "http://localhost:5000/api/v1/todos";
 
-
-useEffect( ()=>{
+  useEffect(() => {
     getTodos();
     // console.log('ihihihi',todos);
-},[])
+  }, []);
 
-async function getTodos(){
+  async function getTodos() {
     const data = await axios.get(api);
     setTodos(data.data.todos);
     console.log(data.data.todos);
+  }
+
+  return (
+    <div>
+      <TodoForm/>
+      {todos.map((el, index) => (
+        <li key={index}>{el.name}</li>
+      ))}
+
+    </div>
+  );
 }
 
-    return (
-        <div>
-            {
-                todos.map((el,index)=><li key={index}>{el.name}</li>)
-            }
-        </div>
-    )
-}
-
-export default TodoList
+export default TodoList;
